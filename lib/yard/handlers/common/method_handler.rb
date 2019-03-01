e3e6990a1e4b913f802834c94d931f2034886ca4
Thomas Thomassen <thomas@thomthom.net>
2019-03-01 11:43:32 +0100

    Fix double return tag.

---
+++ lib/yard/handlers/common/method_handler.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+# Shared functionality between Ruby and C method handlers.
+module YARD::Handlers
+  module Common
+    module MethodHandler
+      # @param [MethodObject] obj
+      def add_predicate_return_tag(obj)
+        if obj.tag(:return) && (obj.tag(:return).types || []).empty?
+          obj.tag(:return).types = ['Boolean']
+        elsif obj.tag(:return).nil?
+          unless obj.tags(:overload).any? {|overload| overload.tag(:return) }
+            obj.add_tag(YARD::Tags::Tag.new(:return, "", "Boolean"))
+          end
+        end
+      end
+    end
+  end
+end
